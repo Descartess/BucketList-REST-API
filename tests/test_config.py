2cdaa3d8e5492fe03d2baf08abb3352efdc6cf17
NyondoPaul <nyondopaul23@gmail.com>
2017-07-18 21:16:43 +0300

    Add tests for config.py

---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+""" unit tests for config.py """
+
+from flask import current_app
+from flask_testing import TestCase
+
+from  app import APP
+
+class TestDevelopmentConfig(TestCase):
+    """ unit tests for Developement config """
+    def create_app(self):
+        """Flask testing intialization """
+        APP.config.from_object('config.DevelopmentConfig')
+        return APP
+
+    def test_app_is_development(self):
+        """ test development configuration """
+        self.assertFalse(current_app is None)
+        self.assertTrue(APP.config['DEBUG'] is True)
+
+
+
+
