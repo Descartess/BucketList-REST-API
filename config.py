257187fa67e2949140bc976b7e99b0586d8e6a02
NyondoPaul <nyondopaul23@gmail.com>
2017-07-19 21:38:05 +0300

    Add SQL Alchemy database url

--- config.py
+++ config.py
@@ -1,19 +1,31 @@
+import os
+
 class BaseConfig:
     """ Base Configuration """
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """ Development configuration """
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """ Testing configuration """
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
+config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'production': ProductionConfig,
 
+    'default': DevelopmentConfig
+}
