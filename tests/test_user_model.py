acc2bc20b37c4354fe3e77254bc23e8b4b7a7875
NyondoPaul <nyondopaul23@gmail.com>
2017-07-20 21:50:29 +0300

    Add user model tests

---
+++ tests/test_user_model.py
@@ -0,0 +1,27 @@
+""" unit tests for the user model """
+from app import db
+from app.models import Users
+from .test_config import ConfigTestCase
+from sqlalchemy.exc import IntegrityError
+
+class UsersTestCase(ConfigTestCase):
+    """ unit tests for the user model """
+    def test_add_user(self):
+        """ Ensure that user is added """
+        user = Users(username="Paul")
+        db.session.add(user)
+        db.session.commit()
+        self.assertTrue(user.id)
+        self.assertEqual(user.username, "Paul")
+
+    def test_duplicate_users(self):
+        """ Ensure that duplicate users are not  added """
+        user = Users(username="Paul")
+        db.session.add(user)
+        db.session.commit()
+        duplicate_user = Users(username="Paul")
+        db.session.add(duplicate_user)
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
+
+
