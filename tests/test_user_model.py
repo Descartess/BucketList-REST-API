f22bffa2469e0897624278344b05924728c22cf6
NyondoPaul <nyondopaul23@gmail.com>
2017-07-23 13:46:11 +0300

    Add missing password/username tests

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -9,7 +9,7 @@ class UsersTestCase(BaseTestCase):
     """ unit tests for the user model """
     def test_add_user(self):
         """ Ensure that user is added """
-        user = Users(username="Paul")
+        user = Users(username="Paul", password="password")
         db.session.add(user)
         db.session.commit()
         self.assertTrue(user.id)
@@ -17,12 +17,24 @@ class UsersTestCase(BaseTestCase):
 
     def test_duplicate_users(self):
         """ Ensure that duplicate users are not  added """
-        user = Users(username="Paul")
+        user = Users(username="Paul", password="pass")
         db.session.add(user)
         db.session.commit()
-        duplicate_user = Users(username="Paul")
+        duplicate_user = Users(username="Paul", password="test")
         db.session.add(duplicate_user)
         with self.assertRaises(IntegrityError):
             db.session.commit()
 
+    def test_missing_password(self):
+        """ Ensure that an error is returned on missing password """
+        user = Users(username="Paul")
+        db.session.add(user)
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
 
+    def test_missing_username(self):
+        """ Ensure that an error is returned on missing username """
+        user = Users(password="Paul")
+        db.session.add(user)
+        with self.assertRaises(IntegrityError):
+            db.session.commit()
