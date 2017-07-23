3f7f07d7ead95434bc9f97b89bda41c0d5214aab
NyondoPaul <nyondopaul23@gmail.com>
2017-07-24 01:26:18 +0300

    Add login tests

--- tests/test_auth_api.py
+++ tests/test_auth_api.py
@@ -1,5 +1,6 @@
 """ unit tests for authentication api """
 import json
+from app.utils import add_user
 from .test_base import BaseTestCase
 
 
@@ -19,7 +20,8 @@ class TestAuthCase(BaseTestCase):
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 201)
-            self.assertIn("Paul", data['message'])
+            self.assertEqual(data['message'], 'Paul')
+            self.assertTrue(data['token'])
 
     def test_register_user_invalid_json(self):
         """ Ensure that error is returned on invalid json """
@@ -30,6 +32,7 @@ class TestAuthCase(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertEqual('Invalid payload', data['message'])
             self.assertEqual('Fail', data['status'])
+            self.assertIsNone(data['token'])
 
     def test_register_user_invalid_keys(self):
         """ Ensure that error is returned on missing/invalid keys """
@@ -42,15 +45,12 @@ class TestAuthCase(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertEqual('Invalid payload', data['message'])
             self.assertEqual('Fail', data['status'])
+            self.assertIsNone(data['token'])
 
     def test_add_duplicate_user(self):
         """ Ensure that an error is returned on a duplicate user registration """
         with self.client:
-            self.client.post(
-                'auth/register',
-                data=json.dumps(dict(username="Paul", password="test")),
-                content_type="application/json"
-            )
+            add_user("Paul", "password")
             response = self.client.post(
                 'auth/register',
                 data=json.dumps(dict(username="Paul", password="test")),
@@ -60,4 +60,42 @@ class TestAuthCase(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertIn("Sorry username already is taken", data['message'])
             self.assertIn("Fail", data['status'])
-            
+            self.assertIsNone(data['token'])
+
+    def test_user_correct_login(self):
+        """ Ensure that user logs in with correct credentials"""
+        with self.client:
+            add_user(username="Paul", password="password")
+            response = self.client.post(
+                'auth/login',
+                data=json.dumps(dict(username="Paul", password="password")),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Success", data['status'])
+
+    def test_user_incorrect_password(self):
+        """ Ensure that user doesnt logs in with incorrect credentials"""
+        with self.client:
+            add_user(username="Paul", password="password")
+            response = self.client.post(
+                'auth/login',
+                data=json.dumps(dict(username="Paul", password="qwerty")),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("Fail", data['status'])
+
+    def test_user_incorrect_username(self):
+        """ Ensure that user doesnt logs in with incorrect credentials"""
+        with self.client:
+            response = self.client.post(
+                'auth/login',
+                data=json.dumps(dict(username="Laura", password="qwerty")),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("Fail", data['status'])
