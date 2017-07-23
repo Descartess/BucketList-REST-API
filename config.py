6923a97a3566d37da76e38e0bc6bc2886afbe7c4
NyondoPaul <nyondopaul23@gmail.com>
2017-07-23 18:36:58 +0300

    Add token, password hashing config

--- config.py
+++ config.py
@@ -5,17 +5,25 @@ class BaseConfig:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "I am here temporarily"
+    BCRYPT_LOG_ROUNDS = 12
+    TOKEN_EXPIRATION_DAYS = 30
+    TOKEN_EXPIRATION_SECONDS = 0
 
 class DevelopmentConfig(BaseConfig):
     """ Development configuration """
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    BCRYPT_LOG_ROUNDS = 4
 
 class TestingConfig(BaseConfig):
     """ Testing configuration """
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    BCRYPT_LOG_ROUNDS = 4
+    TOKEN_EXPIRATION_DAYS = 0
+    TOKEN_EXPIRATION_SECONDS = 5
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
