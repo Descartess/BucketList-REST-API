2eb3a5f42e6f3c416ee0a9749704402f81d4abaf
NyondoPaul <nyondopaul23@gmail.com>
2017-07-24 01:29:35 +0300

    Add login

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,46 +1,68 @@
 """ Authentication functionality """
 from flask import Blueprint, jsonify, request
 from sqlalchemy.exc import IntegrityError
-from app import db
+from app import db, bcrypt
+from app.utils import add_user, validate_auth_json
 from app.models import Users
 
 auth_blueprint = Blueprint('auth', __name__)
 
 
 @auth_blueprint.route('/register', methods=['POST'])
+@validate_auth_json
 def register():
     """ Register a new user """
     post_data = request.get_json()
-    if not post_data:
-        response_object = {
-            'status': 'Fail',
-            'message': 'Invalid payload'
-        }
-        return jsonify(response_object), 400
     username = post_data.get('username')
     password = post_data.get('password')
-    if not username or not password:
-        response_object = {
-            'status': 'Fail',
-            'message': "Invalid payload"
-        }
-        return jsonify(response_object), 400
     try:
         user = Users.query.filter_by(username=username).first()
         if not user:
-            db.session.add(Users(username=username, password=password))
-            db.session.commit()
+            user = add_user(username, password)
             response_object = {
                 'status': 'success',
-                'message': username
+                'message': username,
+                'token': user.encode_auth_token(user.id).decode()
             }
             return jsonify(response_object), 201
         else:
             response_object = {
                 'status': 'Fail',
-                'message': "Sorry username already is taken"
+                'message': "Sorry username already is taken",
+                'token': None
             }
             return jsonify(response_object), 400
     except IntegrityError:
         db.session().rollback()
-        
+
+
+@auth_blueprint.route('/login', methods=["POST"])
+@validate_auth_json
+def login():
+    """ Login in old users """
+    post_data = request.get_json()
+    username = post_data.get('username')
+    password = post_data.get('password')
+    try:
+        user = Users.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            auth_token = user.encode_auth_token(user.id)
+            if auth_token:
+                response_object = {
+                    'status': 'Success',
+                    'token': auth_token.decode()
+                }
+                return jsonify(response_object), 200
+        response_object = {
+            'status': 'Fail',
+            'message': 'Invalid credentials',
+            'token': None
+        }
+        return jsonify(response_object), 400
+    except Exception as e:
+        response_object = {
+            'status': 'Fail',
+            'message': 'Invalid credentials',
+            'token': None
+        }
+        return jsonify(response_object), 400
