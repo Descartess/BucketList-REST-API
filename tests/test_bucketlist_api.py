72a474b3da948f40ba0b832f4816e7f88b06ba17
NyondoPaul <nyondopaul23@gmail.com>
2017-07-24 01:27:57 +0300

    Add post and get route tests

---
+++ tests/test_bucketlist_api.py
@@ -0,0 +1,28 @@
+""" unit tests for the bucketlist api """
+import json
+from .test_base import BaseTestCase
+
+class TestBucketListCase(BaseTestCase):
+    """ unit tests for the bucketlist api """
+    def test_create_bucket_lists(self):
+        """ Ensure that bucketlists can be created """
+        with self.client:
+            response = self.client.post(
+                'bucketlists',
+                data=json.dumps(dict(name="Career", completed_by=30)),
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(data['status'], "Success")
+
+    def test_get_bucket_lists(self):
+        """ Ensure that bucketlists can be retrieved """
+        with self.client:
+            response = self.client.get(
+                'bucketlists',
+                content_type="application/json"
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data['status'], "Success")
