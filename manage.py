24f200a5acc17c8b2129042c75bd50d3d4801b7f
NyondoPaul <nyondopaul23@gmail.com>
2017-07-23 13:51:57 +0300

    Add recreate_db command

--- manage.py
+++ manage.py
@@ -7,5 +7,12 @@ APP = create_app('default')
 
 manager = Manager(APP)
 
+@manager.command
+def recreate_db():
+    """ Recreates a database """
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 if __name__ == "__main__":
     manager.run()
