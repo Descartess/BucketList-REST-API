f9150f14abb978a33dc25b7ac2df6e2db352a830
NyondoPaul <nyondopaul23@gmail.com>
2017-07-23 13:48:57 +0300

    Set field to nullable=False

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ class Users(db.Model):
     """ Model for users """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     bucket_lists = db.relationship('BucketLists', backref="owner", lazy="dynamic")
 
     def __repr__(self):
@@ -15,8 +16,8 @@ class BucketLists(db.Model):
     """ Model for bucket lists """
     __tablename__ = 'bucketlists'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    completed_by = db.Column(db.Integer)
+    name = db.Column(db.String(64), nullable=False)
+    completed_by = db.Column(db.Integer, nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     bucket_lists_items = db.relationship('BucketListItems', backref="bucket_list", lazy="dynamic")
 
@@ -27,8 +28,8 @@ class BucketListItems(db.Model):
     """ Model for bucket list items """
     __tablename__ = "bucketlistitems"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    completed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64), nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable= False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'))
 
     def __repr__(self):
