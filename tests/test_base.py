8a2d76940bdc9b6910d9163b1b402e96fbbc5013
NyondoPaul <nyondopaul23@gmail.com>
2017-07-24 23:45:34 +0300

    Register user on setUp

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,10 +1,13 @@
 """ base set up testing utilities """
 
 import unittest
+import json
 from app import create_app, db
+
 
 class BaseTestCase(unittest.TestCase):
     """ basic configuration for unit tests """
+
     def setUp(self):
         """initial setup before a test is run """
         self.app = create_app('testing')
@@ -12,6 +15,14 @@ class BaseTestCase(unittest.TestCase):
         self.app_context.push()
         self.client = self.app.test_client(self)
         db.create_all()
+        # log in a user
+        resp = self.client.post(
+            'auth/register',
+            data=json.dumps(
+                dict(username='testuser', password='testpassword')),
+            content_type='application/json'
+        )
+        self.token = json.loads(resp.data.decode())['token']
 
     def tearDown(self):
         """executes  after a test is run """
