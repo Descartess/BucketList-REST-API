6bf626ae35153975dbba5723b2981a28e4e5ee96
NyondoPaul <nyondopaul23@gmail.com>
2017-07-24 23:46:47 +0300

    Add auth token tests

--- tests/test_bucketlist_api.py
+++ tests/test_bucketlist_api.py
@@ -10,7 +10,8 @@ class TestBucketListCase(BaseTestCase):
             response = self.client.post(
                 'bucketlists',
                 data=json.dumps(dict(name="Career", completed_by=30)),
-                content_type="application/json"
+                content_type="application/json",
+                headers=dict(Authorization='Bearer '+ self.token)
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 201)
@@ -21,8 +22,21 @@ class TestBucketListCase(BaseTestCase):
         with self.client:
             response = self.client.get(
                 'bucketlists',
-                content_type="application/json"
+                content_type="application/json",
+                headers=dict(Authorization='Bearer '+ self.token)
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(data['status'], "Success")
+            self.assertDictEqual(data, {'bucketlists': []})
+
+    def test_no_auth_token(self):
+        """ Ensure that error is returned with no headers """
+        with self.client:
+            response = self.client.post(
+                'bucketlists',
+                data=json.dumps(dict(name="Career", completed_by=30)),
+                content_type="application/json",
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['message'],'Provide valid auth Token')
+
