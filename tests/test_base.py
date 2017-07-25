d49ae870613d7f9ed8753c8482088e0119099696
NyondoPaul <nyondopaul23@gmail.com>
2017-07-25 15:59:45 +0300

    Code Refactor

--- tests/test_base.py
+++ tests/test_base.py
@@ -15,15 +15,7 @@ class BaseTestCase(unittest.TestCase):
         self.app_context.push()
         self.client = self.app.test_client(self)
         db.create_all()
-        # log in a user
-        resp = self.client.post(
-            'auth/register',
-            data=json.dumps(
-                dict(username='testuser', password='testpassword')),
-            content_type='application/json'
-        )
-        self.token = json.loads(resp.data.decode())['token']
-
+       
     def tearDown(self):
         """executes  after a test is run """
         db.session.remove()
