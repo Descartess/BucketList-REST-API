17d7005f6890d1d87a015d534b62ac456b158c6d
NyondoPaul <nyondopaul23@gmail.com>
2017-07-26 20:28:30 +0300

    Add reset-password, logout tests

--- tests/test_auth_api.py
+++ tests/test_auth_api.py
@@ -1,26 +1,34 @@
 """ unit tests for authentication api """
 import json
+from app import create_app, db
 from app.utils import add_user
 from .test_base import BaseTestCase
 
 
 class TestAuthCase(BaseTestCase):
     """ unit tests for authentication api """
+    def setUp(self):
+        """initial setup before a test is run """
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client(self)
+        db.create_all()
+        # log in a user
+        self.resp = self.client.post(
+            'auth/register',
+            data=json.dumps(
+                dict(username='testuser', password='testpassword')),
+            content_type='application/json'
+        )
+        self.token = json.loads(self.resp.data.decode())['token']
 
     def test_register_user(self):
         """ Ensure that users get registered """
         with self.client:
-            response = self.client.post(
-                'auth/register',
-                data=json.dumps(dict(
-                    username='Paul',
-                    password='test'
-                )),
-                content_type='application/json'
-            )
-            data = json.loads(response.data.decode())
-            self.assertEqual(response.status_code, 201)
-            self.assertEqual(data['message'], 'Paul')
+            data = json.loads(self.resp.data.decode())
+            self.assertEqual(self.resp.status_code, 201)
+            self.assertEqual(data['message'], 'testuser')
             self.assertTrue(data['token'])
 
     def test_register_user_invalid_json(self):
@@ -50,10 +58,9 @@ class TestAuthCase(BaseTestCase):
     def test_add_duplicate_user(self):
         """ Ensure that an error is returned on a duplicate user registration """
         with self.client:
-            add_user("Paul", "password")
             response = self.client.post(
                 'auth/register',
-                data=json.dumps(dict(username="Paul", password="test")),
+                data=json.dumps(dict(username="testuser", password="testpassword")),
                 content_type="application/json"
             )
             data = json.loads(response.data.decode())
@@ -65,10 +72,9 @@ class TestAuthCase(BaseTestCase):
     def test_user_correct_login(self):
         """ Ensure that user logs in with correct credentials"""
         with self.client:
-            add_user(username="Paul", password="password")
             response = self.client.post(
                 'auth/login',
-                data=json.dumps(dict(username="Paul", password="password")),
+                data=json.dumps(dict(username="testuser", password="testpassword")),
                 content_type="application/json"
             )
             data = json.loads(response.data.decode())
@@ -78,10 +84,9 @@ class TestAuthCase(BaseTestCase):
     def test_user_incorrect_password(self):
         """ Ensure that user doesnt logs in with incorrect credentials"""
         with self.client:
-            add_user(username="Paul", password="password")
             response = self.client.post(
                 'auth/login',
-                data=json.dumps(dict(username="Paul", password="qwerty")),
+                data=json.dumps(dict(username="testuser", password="testpword")),
                 content_type="application/json"
             )
             data = json.loads(response.data.decode())
@@ -99,3 +104,45 @@ class TestAuthCase(BaseTestCase):
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
             self.assertIn("Fail", data['status'])
+
+    def test_user_reset_password(self):
+        """ Ensure that user can reset password """
+        with self.client:
+            response = self.client.post(
+                'auth/reset-password',
+                data=json.dumps(
+                    dict(username="testuser", old_password="testpassword", new_password="test")),
+                content_type="application/json",
+                headers=dict(Authorization='Bearer ' + self.token)
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(data['status'], 'Success')
+            self.assertTrue(data['token'])
+
+    def test_user_reset_incorrectpassword(self):
+        """ Ensure that user can reset password """
+        with self.client:
+            response = self.client.post(
+                'auth/reset-password',
+                data=json.dumps(
+                    dict(username="testuser", old_password="password", new_password="test")),
+                content_type="application/json",
+                headers=dict(Authorization='Bearer ' + self.token)
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(data['status'], 'Fail')
+            self.assertIsNone(data['token'])
+
+    def test_user_logout(self):
+        """ Ensure that users can logout successfully """
+        with self.client:
+            response = self.client.get(
+                'auth/logout',
+                content_type="application/json",
+                headers=dict(Authorization='Bearer ' + self.token))
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data['status'], 'Success')
+            
